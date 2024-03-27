c84ecb49511b51b95f0276acbc03a230f7b8e937
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-03-28 01:16:49 +0530

    Add fibonacci series dynamic programming

---
+++ Dynamic-Programming/fibonacci-series.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+long long fibonacci (int n)
+{
+    // use memorization to store the values of the fibonacci series
+    static long long f[100];
+    f[0] = 0;
+    f[1] = 1;
+    for (int i = 2; i < n; i++)
+        f[i] = f[i - 1] + f[i - 2];
+    return f[n - 1];
+}
+
+int main (void)
+{
+    int n;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        printf("%lld ", fibonacci(i));
+    return 0;
+}
